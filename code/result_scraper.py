36c450ac16f97ee8be6b7ce2cc40bf4a26cfe730
Muhammad Ashhad Alam <ashhadalam0030@gmail.com>
2025-05-18 22:27:25 -0700

    Add Fixture fetcher

--- code/result_scraper.py
+++ code/result_scraper.py
@@ -2,23 +2,25 @@ import requests
 import os
 from dotenv import load_dotenv
 import csv
+from collections import defaultdict
+from openpyxl import Workbook
+from pathlib import Path
 
 load_dotenv()
 
 API_KEY = os.getenv("FOOTBALL_DATA_API_KEY")
-BASE_URL = "https://api.football-data.org/v4/competitions/PL/standings"
+BASE_URL = "https://api.football-data.org/v4"
 
-headers = {"X-Auth-Token": API_KEY}
+HEADERS = {"X-Auth-Token": API_KEY}
 
 
-def scrape_results(filename):
-    response = requests.get(BASE_URL, headers=headers)
+def save_current_league_table(filename):
+    url = f"{BASE_URL}/competitions/PL/standings"
+    response = requests.get(url, headers=HEADERS)
     if response.status_code != 200:
-        print("Error")
-        return
+        raise Exception("Failed to get standings")
 
-    data = response.json()
-    standings = data["standings"][0]["table"]
+    standings = response.json()["standings"][0]["table"]
 
     with open(filename, mode="w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
@@ -53,5 +55,40 @@ def scrape_results(filename):
             )
 
 
+def save_latest_matchday(filename):
+    print(filename.resolve())
+    url = f"{BASE_URL}/competitions/PL/matches?season=2024"
+    response = requests.get(url, headers=HEADERS)
+    if response.status_code != 200:
+        raise Exception("Failed to get matches")
+
+    matches = response.json()["matches"]
+    grouped_matches = defaultdict(list)
+    for match in matches:
+        md = match.get("matchday")
+        if md is not None:
+            grouped_matches[md].append(match)
+
+    wb = Workbook()
+    wb.remove(wb.active)
+
+    for matchday, matches in sorted(grouped_matches.items()):
+        sheet = wb.create_sheet(title=f"Matchday {matchday}")
+        sheet.append(["Home Team", "Away Team", "Score", "Status"])
+
+        for match in matches:
+            home = match["homeTeam"]["shortName"]
+            away = match["awayTeam"]["shortName"]
+            status = match["status"]
+
+            if status == "FINISHED":
+                score = f"{match['score']['fullTime']['home']} - {match['score']['fullTime']['away']}"
+            else:
+                score = "vs"
+            sheet.append([home, away, score, status])
+    wb.save(filename)
+
+
 if __name__ == "__main__":
-    scrape_results("..\\results\\PL Standings.csv")
+    # save_current_league_table(Path("results") / "PL" / "PL Standings.csv")
+    save_latest_matchday(Path("results") / "PL" / "Fixtures.xlsx")
